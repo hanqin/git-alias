9bdfe46a4a74cc206715502dda5d6e377a7a1845
Han Qin <hanhaify@gmail.com>
2015-11-19 11:51:12 +0800

    added script for setting up git alias

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # git-alias
 useful git alias, config them with one script
+
+* please check install.sh for details, it's quite simple
+* it will awlayes be WIP, and pull requests are warmly welcomed
---
+++ install.sh
@@ -0,0 +1,5 @@
+git config --global alias.st status
+git config --global alias.pr pull --rebase
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.alias "config --get-regexp ^alias\."
