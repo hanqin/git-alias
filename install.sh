bc0b1dd77cbc55c3e15252092f68cdfff28d9f9f
qinhan <qinhan@xingshulin.com>
2015-11-22 14:59:55 +0800

    added more alias

--- install.sh
+++ install.sh
@@ -3,3 +3,12 @@ git config --global alias.pr pull --rebase
 git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.alias "config --get-regexp ^alias\."
+git config --global alias.amend commit --amend
+git config --global alias.br branch
+git config --global alias.mute update-index --assume-unchanged
+git config --global alias.muted ls-files -v
+git config --global alias.sdc svn dcommit
+git config --global alias.srb svn rebase
+git config --global alias.unmute update-index --no-assume-unchanged
+git config --global alias.unstash stash pop
+
