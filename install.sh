7a8191dcb2235c7c2849d2f24c63e1b0d51a7d1d
qinhan <qinhan@xingshulin.com>
2016-01-19 21:39:49 +0800

    quote long aliases

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
-git config --global alias.st status
-git config --global alias.pr pull --rebase
-git config --global alias.co checkout
-git config --global alias.ci commit
+git config --global alias.st "status"
+git config --global alias.pr "pull --rebase"
+git config --global alias.co "checkout"
+git config --global alias.ci "commit"
 git config --global alias.alias "config --get-regexp ^alias\."
-git config --global alias.amend commit --amend
+git config --global alias.amend "commit --amend"
 git config --global alias.br branch
-git config --global alias.mute update-index --assume-unchanged
-git config --global alias.muted ls-files -v
-git config --global alias.sdc svn dcommit
-git config --global alias.srb svn rebase
-git config --global alias.unmute update-index --no-assume-unchanged
-git config --global alias.unstash stash pop
+git config --global alias.mute "update-index --assume-unchanged"
+git config --global alias.muted "ls-files -v"
+git config --global alias.sdc "svn dcommit"
+git config --global alias.srb "svn rebase"
+git config --global alias.unmute "update-index --no-assume-unchanged"
+git config --global alias.unstash "stash pop"
 
